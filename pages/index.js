bf9f89d3dd666c09f03039ffc1bf09f73309277b
Abdulrahman Azmy <aazmy@adam.ai>
2020-06-13 19:45:25 +0200

    JS const fixes & popup toggle handling

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,32 @@
 //handle pupup show and hide
 const popup = document.querySelector('.popup');
-let editBtn = document.querySelector('.button_edit');
-let closeBtn = document.querySelector('.button_close');
+const editBtn = document.querySelector('.button_type_edit');
+const closeBtn = document.querySelector('.button_type_close');
+const form = document.querySelector('.form');
+const Username = form.querySelector('.form__input_type_name');
+const about = form.querySelector('.form__input_type_about');
+const profileName = document.querySelector('.profile__name');
+const profileAboutMe = document.querySelector('.profile__about-me');
+
 function showpopup(){
-    popup.classList.toggle('popup_is-opened');
+    popup.classList.add('popup_is-opened');
+    popup.style.display = 'flex'
+}
+function hidepopup(){
+    popup.classList.remove('popup_is-opened');
+    popup.style.display = 'none'
 }
 editBtn.addEventListener("click", showpopup);
-closeBtn.addEventListener("click", showpopup);
+closeBtn.addEventListener("click", hidepopup);
 
 //edit profile
-let form = document.querySelector('.form');
 
 function onSubmit(event){
 
     event.preventDefault(); 
-    let name = form.querySelector('.form__input_name');
-    let about = form.querySelector('.form__input_about');
-    let profileName = document.querySelector('.profile__name');
-    let profileAboutMe = document.querySelector('.profile__about-me');
-
-    nameValue= name.value;
-    aboutMeValue= about.value;
-    profileName.textContent = nameValue;
-    profileAboutMe.textContent = aboutMeValue;
-    popup.classList.add('popup_is-opened');
+    profileName.textContent = Username.value;
+    profileAboutMe.textContent = about.value;
+    hidepopup();
 
 }
 form.addEventListener('submit', onSubmit); 
